3dfa6eadb31d6dcee599266ad4ed18d65adf30c4
Naksooroo <31647064+NakSoo@users.noreply.github.com>
2019-05-05 10:15:31 +0900

    7.사과와 오렌지

--- src/test/java/tdd/MoneyTest.java
+++ src/test/java/tdd/MoneyTest.java
@@ -24,11 +24,12 @@ public class MoneyTest {
 	}
 	
 	@Test
-	public void testEQuality() {
+	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
+		assertFalse(new Franc(5).equals(new Dollar(5)));
 	}
 
 }
